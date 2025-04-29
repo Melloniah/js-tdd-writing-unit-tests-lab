5c85318ef31698c324c17af38fc8ec9914fb9612
Mellon Obada <mellobadah123@gmail.com>
2025-04-29 13:14:10 +0300

    codegrade palindrome

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,18 @@
 // Your tests here
+
+import { isPalindrome } from "../utils";
+
+
+describe("isPalindrome", ()=>{
+    it("returns true if the word is a palindrome", ()=>{
+        
+        expect(isPalindrome("racecar")).toBe(true)
+        expect(isPalindrome("mom")).toBe(true)
+        
+    });
+    it("returns false if the word is not a palindrome", ()=>{
+        expect(isPalindrome("car")).toBe(false)
+        expect(isPalindrome("hello")).toBe(false)
+    })
+
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 // Your code here
+
+export function isPalindrome(str){
+    const reversed =str.toLowerCase().split('').reverse().join('')
+    return reversed === str.toLowerCase()
+}
